6c831fe8c9be31b9b435babf92fad3f70c339640
murali-ganta <muralireddy2910@gmail.com>
2024-12-22 13:14:12 -0500

    folder-setup, tailwind css installation

---
+++ frontend/notes-app/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Login />
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
