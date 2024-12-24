328631f6522dc2152df3d7000a6b27f4b8ff9aa1
murali-ganta <muralireddy2910@gmail.com>
2024-12-23 19:04:50 -0500

    added SearchBar UI and hande

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
+import React, { useState } from "react"
 import { ProfileInfo } from "../Cards/ProfileInfo"
 import { useNavigate } from "react-router-dom"
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+    const [serachQuery, setSearchQuery] = useState("")
 
     const navigate = useNavigate;
 
@@ -11,15 +13,29 @@ const Navbar = () => {
         navigate("/login");
     };
 
+    const handleSearch = () => {
+        
+    }
+
+    const onClearSearch = () => {
+        setSearchQuery("");
+    }
+
 
     return(
-        <>
-            <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-                <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+        <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+            <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-                <ProfileInfo onLogout={onLogout} />
-            </div>
-        </>
+            <SearchBar value={serachQuery} 
+                onChange={({target}) => {
+                    setSearchQuery(target.value);
+                }}
+                handleSearch={handleSearch}
+                onClearSearch={onClearSearch}
+            />
+
+            <ProfileInfo onLogout={onLogout} />
+        </div>
     )
 }
 
