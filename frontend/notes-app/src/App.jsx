2a5252be82b0a11f3ab0e64b915acb0dcfcbd622
murali-ganta <muralireddy2910@gmail.com>
2024-12-27 21:35:51 -0500

    added UI and route fixes

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,26 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import SignUp from './pages/SignUp/SignUp'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/dashboard" exact element={<Home />}></Route>
       <Route path="/login" exact element={<Login />}></Route>
       <Route path="/signup" exact element={<SignUp />}></Route>
     </Routes>
   </Router>
-)
+);
 
 const App = () => {
-  return (
-    <div>
-      {routes}
-    </div>
-  )
-}
+  return <div>{routes}</div>;
+};
 
-export default App
+export default App;
--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { ProfileInfo } from "../Cards/ProfileInfo";
+import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
@@ -29,17 +29,19 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-
-      <SearchBar
-        value={searchQuery}
-        onChange={({ target }) => {
-          setSearchQuery(target.value);
-        }}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch}
-      />
-
-      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+      {userInfo && (
+        <>
+          <SearchBar
+            value={searchQuery}
+            onChange={({ target }) => {
+              setSearchQuery(target.value);
+            }}
+            handleSearch={handleSearch}
+            onClearSearch={onClearSearch}
+          />
+          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+        </>
+      )}
     </div>
   );
 };
